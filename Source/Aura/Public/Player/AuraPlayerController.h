cf23e89355583969764645f07fc41e0fa9ad91a9
Thiago L. Zippinotti <thiago.lozzer@gmail.com>
2024-01-09 07:08:47 -0300

    Added enhanced input module to the project, AuraPlayerController class, and move input action

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,27 @@
+// Copyright Luma Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	TObjectPtr<UInputMappingContext> AuraContext;
+};
